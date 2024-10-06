a94950b9c05a8c63e12f15aef8c204033a2ae4b8
feyo <kenenisafeyisa@gmail.com>
2024-10-06 13:39:18 -0800

    add custom handler

--- main.go
+++ main.go
@@ -12,12 +12,12 @@ import (
 
 
 func main()  {
-	//another way of starting server
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/",getRoot)
-	
-	err1 := http.ListenAndServe(":8080",http.FileServer(http.Dir("./")))
 
+	mux := http.NewServeMux()
+	mux.Handle("/",http.FileServer(http.Dir("./")))
+	mux.HandleFunc("/healthz",isHealth)	
+	
+	err1 := http.ListenAndServe(":8080",mux)
 	if errors.Is(err1, http.ErrServerClosed){
 		fmt.Printf("Server closed\n")
 	}else if err1 != nil{
@@ -31,5 +31,13 @@ func main()  {
 func getRoot( w http.ResponseWriter,r *http.Request){
 	fmt.Printf("got / request\n")
 	io.WriteString(w, "I am testing my website\n")
+
+}
+
+
+func isHealth (w http.ResponseWriter,r *http.Request){
+	w.Header().Add("Content-Type","text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 
 }
