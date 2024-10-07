cae2590598fc8c0dc0dae703faab82c20aee7738
afeyisa <kenenisafeyisa@gmail.com>
2024-10-07 13:01:31 +0300

    middle ware phase

--- main.go
+++ main.go
@@ -6,16 +6,33 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
+	"sync/atomic"
 )
 
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
+var x = apiConfig{fileserverHits: atomic.Int32{}}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return  http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main()  {
 
+	
 	mux := http.NewServeMux()
-	mux.Handle("/",http.FileServer(http.Dir("./")))
+
 	mux.HandleFunc("/healthz",isHealth)	
+	mux.HandleFunc("/rf",getNumberOfReq)
+	mux.Handle("/",	x.middlewareMetricsInc(http.FileServer(http.Dir("./"))))
+
 	
 	err1 := http.ListenAndServe(":8080",mux)
 	if errors.Is(err1, http.ErrServerClosed){
@@ -39,5 +56,13 @@ func isHealth (w http.ResponseWriter,r *http.Request){
 	w.Header().Add("Content-Type","text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+
+}
+
+func getNumberOfReq(w http.ResponseWriter,r *http.Request){
+	w.Header().Add("Content-Type","text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	w.Write([]byte(strconv.Itoa(int(x.fileserverHits.Load()))))
 
 }
